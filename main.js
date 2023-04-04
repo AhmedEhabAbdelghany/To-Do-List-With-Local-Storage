8208d95fa0766ea3f40481e089644909106456e8
Ahmed Ehab Abdelghany <102571403+AhmedEhabAbdelghany@users.noreply.github.com>
2023-04-04 23:15:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// Add Data
+var input  = document.getElementById("input")
+var button = document.getElementById("btn")
+var displaydata = document.getElementById("containerofalldata")
+
+
+
+var todolist=[]
+button.addEventListener("click",function(){
+    if(input.value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+    var todoinput = {
+        tId:todolist.length==0? 0 : todolist.length,
+        todo:input.value,
+    }
+    todolist.push(todoinput)
+    localStorage.setItem("To Do List", JSON.stringify(todolist))
+    input.value = "";
+    display(todolist)
+    }
+})
+if(JSON.parse(localStorage.getItem("To Do List")) != null){
+    todolist = JSON.parse(localStorage.getItem("To Do List"));
+    display(todolist)
+}
+
+// For Display Data
+function display (view){
+    var todocontainer = "";
+    for(var i = 0; i<view.length ; i++)
+    {
+        todocontainer += `
+        <table class="outputtable"><tr> <td><input type="checkbox" id="todocheck" class="checkmark" onClick="donetodo(${view[i].tId})"></td><td><p id="mypar">${view[i].todo}</p></td><td><button class="delete" onClick="deletetodo(${view[i].tId})">Delete</button></td></tr></table>
+        `
+    }
+    displaydata.innerHTML = todocontainer
+}
+
+
+
+
+
+// Done
+function donetodo(id) {
+    var pindex = todolist.findIndex(gettodoindex)
+    function gettodoindex(item){
+        return item.tId == id;
+    }
+    var mypara = todolist[pindex].todo;
+    console.log(mypara)
+}
+
+
+
+
+// Delete Data
+function deletetodo(id) {
+    var pindex = todolist.findIndex(gettodoindex)
+    function gettodoindex(item){
+        return item.tId == id;
+    }
+    todolist.splice(pindex,1);
+    localStorage.setItem("To Do List", JSON.stringify(todolist))
+    display(todolist)
+}
+
